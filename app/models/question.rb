0ac1a4902ed060ed855ff57fdbfc59eeec98c53e
Askar Zinurov <askar.zinurov@flatsoft.com>
2009-06-30 17:52:26 +0400

    Добавлена сериализация правильных ответов в question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :exam
   validates_presence_of :question, :type
   validates_length_of :type, :within => 3..250
-  validates_inclusion_of :type, :in => %w{multi, single}
+  validates_inclusion_of :type, :in => %w{multi single}
+  serialize :answers_list
 end

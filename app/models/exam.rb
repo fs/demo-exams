306dcc715c4c4a4919783b3e01e12509acb0b1ef
Timur Vafin <timur.vafin@flatsoft.com>
2009-06-30 18:54:08 +0400

    Cleanup, [\#1 state:accepted]

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,6 +2,7 @@ class Exam < ActiveRecord::Base
   has_many :questions
   has_many :user_exams
   has_many :users, :through => :user_exams
+  
   validates_presence_of :name, :question_count, :time_limit
   validates_length_of :name, :within => 3..250
   validates_numericality_of :question_count, :time_limit

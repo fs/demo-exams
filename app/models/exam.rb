09eb66681a1a93fbc0ea18fa2dc544f92f6c4b12
Askar Zinurov <askar.zinurov@flatsoft.com>
2009-06-30 18:42:52 +0400

    Exam - User relationship updated [\#1 tagged:committed state:done]

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,14 +1,12 @@
 class Exam < ActiveRecord::Base
   has_many :questions
-  validates_presence_of :name, :question_count, :time_limit 
+  has_many :user_exams
+  has_many :users, :through => :user_exams
+  validates_presence_of :name, :question_count, :time_limit
   validates_length_of :name, :within => 3..250
   validates_numericality_of :question_count, :time_limit
 
   def start(user)
-    user_exam = UserExam.new
-    user_exam.user_id = user.id
-    user_exam.exam_id = self.id
-    user_exam.save!
-    user_exam
+    users << user
   end
 end

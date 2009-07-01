63bf6ccb15d07a6d0f4a64610f961bbbd5096833
Timur Vafin <timur.vafin@flatsoft.com>
2009-07-01 17:21:44 +0400

    Added test helper and factories

---
+++ test/factories/exam.rb
@@ -0,0 +1,6 @@
+Factory.define :exam do |exam|
+  exam.name 'Git basics'
+  exam.description 'Git basics'
+  exam.question_count 5
+  exam.time_limit 10
+end
---
+++ test/factories/user_exam.rb
@@ -0,0 +1,19 @@
+Factory.define :user_exam do |user_exam|
+  user_exam.association :user, :factory => :user
+  user_exam.association :exam, :factory => :exam
+  user_exam.finished_count 0
+end
+
+Factory.define :user_exam_expired, :parent => :user_exam do |user_exam|
+  user_exam.finished_at nil
+  user_exam.created_at 30.minutes.ago
+end
+
+Factory.define :user_exam_finished, :parent => :user_exam do |user_exam|
+  user_exam.finished_count 5
+  user_exam.finished_at 10.minutes.ago
+end
+
+Factory.define :user_exam_with_last_question, :parent => :user_exam do |user_exam|
+  user_exam.finished_count 4
+end

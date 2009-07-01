e640a2942073622c7cfa9189759a6a2b91b15b27
Timur Vafin <timur.vafin@flatsoft.com>
2009-07-01 17:27:18 +0400

    Refactored answer! method [#4]

--- app/models/user_question.rb
+++ app/models/user_question.rb
@@ -6,11 +6,13 @@ class UserQuestion < ActiveRecord::Base
   # Set correct answer and updates user exams finished attributes
   #
   def answer!(answers)
-    if !user_exam.expired? && !user_exam.finished?
-      if answers.sort == question.answers_list
-        update_attribute(:correct, true)
-        user_exam.correct_answer!
-      end
+    return false if user_exam.expired? || user_exam.finished?
+
+    if answers.sort == question.answers_list
+      update_attribute(:correct, true)
+      user_exam.correct_answer!
     end
+
+    true
   end
 end

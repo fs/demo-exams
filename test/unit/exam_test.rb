bf3cc4af90eb1b8f1d1a1503960d524c2ba4d629
Timur Vafin <timur.vafin@flatsoft.com>
2009-07-01 17:21:17 +0400

    Added test for exam model [#18 state:accepted]

--- test/unit/exam_test.rb
+++ test/unit/exam_test.rb
@@ -1,8 +1,12 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class ExamTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should_have_many :questions
+  should_have_many :user_exams
+  should_have_many :users, :through => :user_exams
+
+  should_validate_presence_of :name, :question_count, :time_limit
+  should_validate_numericality_of :question_count
+  should_validate_numericality_of :time_limit
+  should_ensure_length_in_range :name, (3..250)
 end

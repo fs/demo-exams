942c8471b8332eccc78d6746a04e90d326aed05c
Askar Zinurov <askar.zinurov@flatsoft.com>
2009-07-03 17:34:59 +0400

    Added expired? test before accept users answer [#11 state:qa_needed responsible:'Timur Vafin']

--- app/controllers/user_questions_controller.rb
+++ app/controllers/user_questions_controller.rb
@@ -5,18 +5,17 @@ class UserQuestionsController < ApplicationController
 
   def update
     @user_question = UserQuestion.find(params[:id])
+    return redirect_to(user_exams_path) if @user_question.user_exam.expired?
     answers = []
     (1..5).each do |i|
       ans_sym = ('answer_' + i.to_s).to_sym
       answers.push i unless params[ans_sym].nil?
     end
-    @user_question.answer!(answers.sort)
     next_question = @user_question.user_exam.next_question(@user_question)
-    if next_question.nil?
-      redirect_to(user_exams_path)
-    else
-      redirect_to(edit_user_question_path( next_question))
-    end
+    @user_question.answer!(answers.sort)
+
+    return redirect_to(user_exams_path) if next_question.nil?
+    redirect_to(edit_user_question_path( next_question))
   end
 
 end

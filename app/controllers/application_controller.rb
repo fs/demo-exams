1d17c93c64a26d709c3dfbe08af112538e097fd5
Askar Zinurov <askar.zinurov@flatsoft.com>
2009-07-06 16:24:10 +0400

    Clearance User authorization [#13 responsible:'Timur Vafin' state:qa_needed]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,5 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-
-  def current_user
-    @current_user ||= User.first
-  end
+  
 end
--- app/controllers/user_exams_controller.rb
+++ app/controllers/user_exams_controller.rb
@@ -1,9 +1,12 @@
 class UserExamsController < ApplicationController
+  before_filter :authenticate
+
   def index
     @user_exams = current_user.user_exams
   end
 
   def create
+    authenticate && return if current_user.nil?
     user_exam = UserExam.start!(current_user, Exam.find(params[:user_exam][:exam_id]))
     redirect_to(edit_user_question_path(user_exam.next_question))
   end

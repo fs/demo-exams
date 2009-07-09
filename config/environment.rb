a40d107cc43e3e5b50995841200e012b69976498
Timur Vafin <timur.vafin@flatsoft.com>
2009-07-09 13:22:41 +0400

    Added app initializers

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
-DO_NOT_REPLY = "askar.zinurov@flatsoft.com"
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
---
+++ config/initializers/1_clearance.rb
@@ -0,0 +1,2 @@
+HOST = APP_CONFIG['host']
+DO_NOT_REPLY = APP_CONFIG['mails']['noreply']

e917c0270935a86b12593393c0f7531beb34d915
Askar Zinurov <askar.zinurov@flatsoft.com>
2009-07-13 15:46:44 +0400

    Remove unused files

--- test/functional/exam_questions_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ExamQuestionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- test/unit/helpers/exam_questions_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ExamQuestionsHelperTest < ActionView::TestCase
-end

3dbc0c4a70cd4c4848163a7544635abb605c2353
Timur Vafin <timur.vafin@flatsoft.com>
2009-07-23 14:16:27 +0400

    Cleanup views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def if_admin?
+    yield if current_user.admin?
+  end
+
+  def main_menu_item(name, url, logo = nil, with_logo = true)
+    logo = logo && with_logo ? image_tag(logo) : ''
+
+    link_to(
+      logo + content_tag(:span, name),
+      url,
+      current_page?(url) ? {:class => 'current'} : {}
+    )
+  end
 end

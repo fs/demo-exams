d711d307da611a6ca44be1a51e57bf19fb7ad520
Anton Rogov <anton.s.rogov@gmail.com>
2009-09-09 20:08:21 +0400

    fixing capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "exams"
 set :repository,  "git@github.com:fs/rails-exams.git"
+set :scm, :git
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -13,3 +14,29 @@ set :deploy_to, "/var/www/rails/#{application}"
 role :app, "exams.flatsoft.com"
 role :web, "exams.flatsoft.com"
 role :db,  "exams.flatsoft.com", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+
+  desc "Run this x every successful deployment"
+  task :after_default do
+    cleanup
+  end
+end
+
+namespace :db do
+  desc "Create database password in shared path"
+  task :password do
+    set :db_password, Proc.new { Capistrano::CLI.password_prompt("Remote database password: ") }
+    run "mkdir -p #{shared_path}/config"
+    put db_password, "#{shared_path}/config/dbpassword"
+  end
+end
